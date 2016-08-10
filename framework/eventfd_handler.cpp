90fe5f18545178083059a4acc8b40f5eab9f6966
lixingyi <lixingyi@mmbang.net>
2016-08-10 11:17:46 +0800

    fix bug

--- framework/eventfd_handler.cpp
+++ framework/eventfd_handler.cpp
@@ -55,8 +55,8 @@ int eventfd_handler::notify(int64_t v)
 
 void eventfd_handler::on_read(int fd)
 {
-    int64_t v ;
-    read(m_event_fd,(char*)&v,sizeof(v)) ;
+    uint64_t v ;
+    read(m_event_fd,&v,sizeof(v)) ;
     m_callback(v) ;
 }
 
